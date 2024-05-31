83915c5e94e3de05717c9e70600e63fa48e00db6
Ben Huckvale <5511911+benhuckvale@users.noreply.github.com>
2024-05-31 13:42:40 +0100

    Add show_colours function

--- zsh/colours.zsh
+++ zsh/colours.zsh
@@ -20,3 +20,25 @@ BBlue=$'%{\e[1;34m%}'
 BPurple=$'%{\e[1;35m%}'
 BCyan=$'%{\e[1;36m%}'
 BWhite=$'%{\e[1;37m%}'
+
+
+show_colours() {
+    echo "Showing available colour variables"
+    print -P "${NoColour}NoColour"
+    print -P "${Black}Black"
+    print -P "${Red}Red"
+    print -P "${Green}Green"
+    print -P "${Yellow}Yellow"
+    print -P "${Blue}Blue"
+    print -P "${Purple}Purple"
+    print -P "${Cyan}Cyan"
+    print -P "${White}White"
+    print -P "${BBlack}BBlack (Bold Black)"
+    print -P "${BRed}BRed (Bold Red)"
+    print -P "${BGreen}BGreen (Bold Green)"
+    print -P "${BYellow}BYellow (Bold Yellow)"
+    print -P "${BBlue}BBlue (Bold Blue)"
+    print -P "${BPurple}BPurple (Bold Purple)"
+    print -P "${BCyan}BCyan (Bold Cyan)"
+    print -P "${BWhite}BWhite (Bold White)"
+}
