17f816bd55735ecfde7fd7a26532ee52fb61b847
Ben Huckvale <5511911+benhuckvale@users.noreply.github.com>
2024-05-31 12:20:16 +0100

    Add colours.zsh

---
+++ zsh/colours.zsh
@@ -0,0 +1,22 @@
+# Reset
+NoColour=$'%{\e[0m%}'
+
+# Regular Colors
+Black=$'%{\e[0;30m%}'
+Red=$'%{\e[0;31m%}'
+Green=$'%{\e[0;32m%}'
+Yellow=$'%{\e[0;33m%}'
+Blue=$'%{\e[0;34m%}'
+Purple=$'%{\e[0;35m%}'
+Cyan=$'%{\e[0;36m%}'
+White=$'%{\e[0;37m%}'
+
+# Bold
+BBlack=$'%{\e[1;30m%}'
+BRed=$'%{\e[1;31m%}'
+BGreen=$'%{\e[1;32m%}'
+BYellow=$'%{\e[1;33m%}'
+BBlue=$'%{\e[1;34m%}'
+BPurple=$'%{\e[1;35m%}'
+BCyan=$'%{\e[1;36m%}'
+BWhite=$'%{\e[1;37m%}'
